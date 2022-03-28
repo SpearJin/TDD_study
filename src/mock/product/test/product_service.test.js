843548fcc796a4656b0d389b1e22ab39d05f2326
SpearJin <ckdwls2525@gmail.com>
2022-03-28 12:38:17 +0900

    product

---
+++ src/mock/product/test/product_service.test.js
@@ -0,0 +1,18 @@
+const ProductService = require('../product_service.js');
+const StubProductClient = require('./stub_product_client.js');
+
+describe('ProductService - Stub', () => {
+  let productService;
+
+  beforeEach(() => {
+    productService = new ProductService(new StubProductClient());
+  });
+
+  it('should filter out only available items', async () => {
+    const items = await productService.fetchAvailableItems();
+    expect(items.length).toBe(1);
+    expect(items).toEqual[
+      ({ item: '🥛', available: true }, { item: '🍌', available: false })
+    ];
+  });
+});

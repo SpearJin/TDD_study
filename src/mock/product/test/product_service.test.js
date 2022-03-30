d30a64f2bc10e3ebd4aa9d36f28f921c46fbd8cb
SpearJin <ckdwls2525@gmail.com>
2022-03-30 15:35:18 +0900

    stack 1 test

--- src/mock/product/test/product_service.test.js
+++ src/mock/product/test/product_service.test.js
@@ -5,11 +5,15 @@ describe('ProductService - Stub', () => {
   let productService;
 
   beforeEach(() => {
+    // Arrange, Given 준비 단계
     productService = new ProductService(new StubProductClient());
   });
 
   it('should filter out only available items', async () => {
+    // Act, When 실행 단계
     const items = await productService.fetchAvailableItems();
+
+    // Assert, Then 검증 단계
     expect(items.length).toBe(1);
     expect(items).toEqual[
       ({ item: '🥛', available: true }, { item: '🍌', available: false })
